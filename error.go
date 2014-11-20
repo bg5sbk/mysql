34628ae4506c995a93e0c6a24833251871a042d4
idada <bg5sbk@gmail.com>
2014-11-20 16:11:42 +0800

    add comment.

--- error.go
+++ error.go
@@ -6,12 +6,14 @@ package mysql
 import "C"
 import "fmt"
 
+// SQL error.
 type SqlError struct {
 	Num     int
 	Message string
 	Query   string
 }
 
+// Get error string.
 func (se *SqlError) Error() string {
 	if se.Query == "" {
 		return fmt.Sprintf("%v (errno %v)", se.Message, se.Num)
@@ -19,6 +21,7 @@ func (se *SqlError) Error() string {
 	return fmt.Sprintf("%v (errno %v) during query: %s", se.Message, se.Num, se.Query)
 }
 
+// Get error number.
 func (se *SqlError) Number() int {
 	return se.Num
 }
@@ -30,12 +33,14 @@ func (conn *Connection) lastError(query string) error {
 	return &SqlError{0, "Unknow", string(query)}
 }
 
+// Statement error.
 type StmtError struct {
 	Num     int
 	Message string
 	Stmt    *Stmt
 }
 
+// Get error string.
 func (self *StmtError) Error() string {
 	if len(self.Stmt.sql) == 0 {
 		return fmt.Sprintf("%v (errno %v)", self.Message, self.Num)
@@ -43,6 +48,7 @@ func (self *StmtError) Error() string {
 	return fmt.Sprintf("%v (errno %v) during query: %s", self.Message, self.Num, self.Stmt.sql)
 }
 
+// Get error number.
 func (stmt *Stmt) lastError() error {
 	if err := C.my_stmt_error(&stmt.s); *err != 0 {
 		return &StmtError{Num: int(C.my_stmt_errno(&stmt.s)), Message: C.GoString(C.my_stmt_error(&stmt.s)), Stmt: stmt}
