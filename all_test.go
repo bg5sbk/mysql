c2ec63759ff3c28dcd98af7fbb89859dcff07085
idada <bg5sbk@gmail.com>
2015-12-01 00:43:53 +0800

    适配新版unitest

--- all_test.go
+++ all_test.go
@@ -30,10 +30,10 @@ func Test_Connect(t *testing.T) {
 	param.DbName = "mysql"
 
 	conn, err := Connect(param)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	conn.Close()
-	unitest.Pass(t, conn.IsClosed())
+	unitest.Assert(t, conn.IsClosed())
 }
 
 func Test_Execute(t *testing.T) {
@@ -41,78 +41,78 @@ func Test_Execute(t *testing.T) {
 	param.DbName = "mysql"
 
 	conn, err := Connect(param)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	_, err = conn.Execute("CREATE DATABASE " + TestConnParam.DbName)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	_, err = conn.Execute("USE " + TestConnParam.DbName)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	_, err = conn.Execute(`CREATE TABLE test (
 		id INT PRIMARY KEY,
 		value VARCHAR(10)
 	)`)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	for i := 0; i < 10; i++ {
 		res, err := conn.Execute("INSERT INTO test VALUES(" + strconv.Itoa(i) + ",'" + strconv.Itoa(i) + "')")
-		unitest.NotError(t, err)
-		unitest.Pass(t, res.RowsAffected() == 1)
+		unitest.AssertNotError(t, err)
+		unitest.AssertInt64(t, res.RowsAffected(), "==", 1)
 	}
 }
 
 func Test_QueryTable(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	var res DataTable
 
 	res, err = conn.QueryTable("SELECT * FROM test ORDER BY id ASC")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	rows := res.Rows()
-	unitest.Pass(t, len(rows) == 10)
+	unitest.AssertInt(t, len(rows), "==", 10)
 
 	for i := 0; i < 10; i++ {
-		unitest.Pass(t, rows[i][0].Int64() == int64(i))
-		unitest.Pass(t, rows[i][1].String() == strconv.Itoa(i))
+		unitest.AssertInt64(t, rows[i][0].Int64(), "==", int64(i))
+		unitest.AssertString(t, rows[i][1].String(), strconv.Itoa(i))
 	}
 }
 
 func Test_QueryReader(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	var res DataReader
 
 	res, err = conn.QueryReader("SELECT * FROM test ORDER BY id ASC")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer res.Close()
 
 	i := 0
 	for {
 		row, err1 := res.FetchNext()
-		unitest.NotError(t, err1)
+		unitest.AssertNotError(t, err1)
 
 		if row == nil {
 			break
 		}
 
-		unitest.Pass(t, row[0].Int64() == int64(i))
-		unitest.Pass(t, row[1].String() == strconv.Itoa(i))
+		unitest.AssertInt64(t, row[0].Int64(), "==", int64(i))
+		unitest.AssertString(t, row[1].String(), strconv.Itoa(i))
 		i++
 	}
 
-	unitest.Pass(t, i == 10)
+	unitest.AssertInt(t, i, "==", 10)
 }
 
 func Test_Prepare(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	var (
@@ -123,81 +123,81 @@ func Test_Prepare(t *testing.T) {
 	)
 
 	stmt, err = conn.Prepare("SELECT * FROM test ORDER BY id ASC")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	table, err = stmt.QueryTable()
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	rows := table.Rows()
-	unitest.Pass(t, len(rows) == 10)
+	unitest.AssertInt(t, len(rows), "==", 10)
 
 	for i := 0; i < 10; i++ {
-		unitest.Pass(t, rows[i][0].Int64() == int64(i))
-		unitest.Pass(t, rows[i][1].String() == strconv.Itoa(i))
+		unitest.AssertInt64(t, rows[i][0].Int64(), "==", int64(i))
+		unitest.AssertString(t, rows[i][1].String(), strconv.Itoa(i))
 	}
 
 	reader, err = stmt.QueryReader()
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	i := 0
 	for {
 		row, err1 := reader.FetchNext()
-		unitest.NotError(t, err1)
+		unitest.AssertNotError(t, err1)
 
 		if row == nil {
 			break
 		}
 
-		unitest.Pass(t, row[0].Int64() == int64(i))
-		unitest.Pass(t, row[1].String() == strconv.Itoa(i))
+		unitest.AssertInt64(t, row[0].Int64(), "==", int64(i))
+		unitest.AssertString(t, row[1].String(), strconv.Itoa(i))
 
 		i++
 	}
 
-	unitest.Pass(t, i == 10)
+	unitest.AssertInt(t, i, "==", 10)
 
 	stmt.Close()
 
 	stmt, err = conn.Prepare("INSERT INTO test VALUES(?, ?)")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	stmt.BindInt(10)
 	stmt.BindText("10")
 
 	res, err = stmt.Execute()
-	unitest.NotError(t, err)
-	unitest.Pass(t, res.RowsAffected() == 1)
+	unitest.AssertNotError(t, err)
+	unitest.AssertInt64(t, res.RowsAffected(), "==", 1)
 
 	stmt.Close()
 }
 
 func Test_Null(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	var res DataTable
 
 	res, err = conn.QueryTable("SELECT SUM(value) FROM test")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	rows1 := res.Rows()
-	unitest.Pass(t, len(rows1) == 1)
-	unitest.Pass(t, rows1[0][0].IsNull() == false)
+	unitest.AssertInt(t, len(rows1), "==", 1)
+	unitest.Assert(t, !rows1[0][0].IsNull())
 
 	res, err = conn.QueryTable("SELECT SUM(value) FROM test WHERE id > 8888")
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 
 	rows2 := res.Rows()
-	unitest.Pass(t, len(rows2) == 1)
-	unitest.Pass(t, rows2[0][0].IsNull())
+	unitest.AssertInt(t, len(rows2), "==", 1)
+	unitest.Assert(t, rows2[0][0].IsNull())
 }
 
 func Test_Clean(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 	defer conn.Close()
 
 	_, err = conn.Execute("DROP DATABASE " + TestConnParam.DbName)
-	unitest.NotError(t, err)
+	unitest.AssertNotError(t, err)
 }
