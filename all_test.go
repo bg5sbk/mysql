d19284b06cef295e6763889c145f90b7755fdf6f
dada <bg5sbk@gmail.com>
2015-12-01 12:59:26 +0800

    配合单元测试工具调整

--- all_test.go
+++ all_test.go
@@ -1,7 +1,7 @@
 package mysql
 
 import (
-	"github.com/funny/unitest"
+	"github.com/funny/utest"
 	"os"
 	"strconv"
 	"testing"
@@ -30,10 +30,10 @@ func Test_Connect(t *testing.T) {
 	param.DbName = "mysql"
 
 	conn, err := Connect(param)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	conn.Close()
-	unitest.Assert(t, conn.IsClosed())
+	utest.Assert(t, conn.IsClosed())
 }
 
 func Test_Execute(t *testing.T) {
@@ -41,78 +41,78 @@ func Test_Execute(t *testing.T) {
 	param.DbName = "mysql"
 
 	conn, err := Connect(param)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	_, err = conn.Execute("CREATE DATABASE " + TestConnParam.DbName)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	_, err = conn.Execute("USE " + TestConnParam.DbName)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	_, err = conn.Execute(`CREATE TABLE test (
 		id INT PRIMARY KEY,
 		value VARCHAR(10)
 	)`)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	for i := 0; i < 10; i++ {
 		res, err := conn.Execute("INSERT INTO test VALUES(" + strconv.Itoa(i) + ",'" + strconv.Itoa(i) + "')")
-		unitest.AssertNotError(t, err)
-		unitest.AssertInt64(t, res.RowsAffected(), "==", 1)
+		utest.IsNilNow(t, err)
+		utest.EqualNow(t, res.RowsAffected(), 1)
 	}
 }
 
 func Test_QueryTable(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var res DataTable
 
 	res, err = conn.QueryTable("SELECT * FROM test ORDER BY id ASC")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	rows := res.Rows()
-	unitest.AssertInt(t, len(rows), "==", 10)
+	utest.EqualNow(t, len(rows), 10)
 
 	for i := 0; i < 10; i++ {
-		unitest.AssertInt64(t, rows[i][0].Int64(), "==", int64(i))
-		unitest.AssertString(t, rows[i][1].String(), strconv.Itoa(i))
+		utest.EqualNow(t, rows[i][0].Int64(), int64(i))
+		utest.EqualNow(t, rows[i][1].String(), strconv.Itoa(i))
 	}
 }
 
 func Test_QueryReader(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var res DataReader
 
 	res, err = conn.QueryReader("SELECT * FROM test ORDER BY id ASC")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer res.Close()
 
 	i := 0
 	for {
 		row, err1 := res.FetchNext()
-		unitest.AssertNotError(t, err1)
+		utest.IsNilNow(t, err1)
 
 		if row == nil {
 			break
 		}
 
-		unitest.AssertInt64(t, row[0].Int64(), "==", int64(i))
-		unitest.AssertString(t, row[1].String(), strconv.Itoa(i))
+		utest.EqualNow(t, row[0].Int64(), int64(i))
+		utest.EqualNow(t, row[1].String(), strconv.Itoa(i))
 		i++
 	}
 
-	unitest.AssertInt(t, i, "==", 10)
+	utest.EqualNow(t, i, 10)
 }
 
 func Test_Prepare(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var (
@@ -123,81 +123,81 @@ func Test_Prepare(t *testing.T) {
 	)
 
 	stmt, err = conn.Prepare("SELECT * FROM test ORDER BY id ASC")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	table, err = stmt.QueryTable()
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	rows := table.Rows()
-	unitest.AssertInt(t, len(rows), "==", 10)
+	utest.EqualNow(t, len(rows), 10)
 
 	for i := 0; i < 10; i++ {
-		unitest.AssertInt64(t, rows[i][0].Int64(), "==", int64(i))
-		unitest.AssertString(t, rows[i][1].String(), strconv.Itoa(i))
+		utest.EqualNow(t, rows[i][0].Int64(), int64(i))
+		utest.EqualNow(t, rows[i][1].String(), strconv.Itoa(i))
 	}
 
 	reader, err = stmt.QueryReader()
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	i := 0
 	for {
 		row, err1 := reader.FetchNext()
-		unitest.AssertNotError(t, err1)
+		utest.IsNilNow(t, err1)
 
 		if row == nil {
 			break
 		}
 
-		unitest.AssertInt64(t, row[0].Int64(), "==", int64(i))
-		unitest.AssertString(t, row[1].String(), strconv.Itoa(i))
+		utest.EqualNow(t, row[0].Int64(), int64(i))
+		utest.EqualNow(t, row[1].String(), strconv.Itoa(i))
 
 		i++
 	}
 
-	unitest.AssertInt(t, i, "==", 10)
+	utest.EqualNow(t, i, 10)
 
 	stmt.Close()
 
 	stmt, err = conn.Prepare("INSERT INTO test VALUES(?, ?)")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	stmt.BindInt(10)
 	stmt.BindText("10")
 
 	res, err = stmt.Execute()
-	unitest.AssertNotError(t, err)
-	unitest.AssertInt64(t, res.RowsAffected(), "==", 1)
+	utest.IsNilNow(t, err)
+	utest.EqualNow(t, res.RowsAffected(), 1)
 
 	stmt.Close()
 }
 
 func Test_Null(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var res DataTable
 
 	res, err = conn.QueryTable("SELECT SUM(value) FROM test")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	rows1 := res.Rows()
-	unitest.AssertInt(t, len(rows1), "==", 1)
-	unitest.Assert(t, !rows1[0][0].IsNull())
+	utest.EqualNow(t, len(rows1), 1)
+	utest.Assert(t, !rows1[0][0].IsNull())
 
 	res, err = conn.QueryTable("SELECT SUM(value) FROM test WHERE id > 8888")
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 
 	rows2 := res.Rows()
-	unitest.AssertInt(t, len(rows2), "==", 1)
-	unitest.Assert(t, rows2[0][0].IsNull())
+	utest.EqualNow(t, len(rows2), 1)
+	utest.Assert(t, rows2[0][0].IsNull())
 }
 
 func Test_Clean(t *testing.T) {
 	conn, err := Connect(TestConnParam)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	_, err = conn.Execute("DROP DATABASE " + TestConnParam.DbName)
-	unitest.AssertNotError(t, err)
+	utest.IsNilNow(t, err)
 }
--- driver/driver_test.go
+++ driver/driver_test.go
@@ -3,7 +3,7 @@ package driver
 import (
 	"database/sql"
 	"encoding/json"
-	"github.com/funny/unitest"
+	"github.com/funny/utest"
 	"os"
 	"strconv"
 	"testing"
@@ -41,10 +41,10 @@ func init() {
 
 func Test_Connect(t *testing.T) {
 	conn, err := sql.Open("mysql", TestConnParam)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	err = conn.Close()
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 }
 
 func Test_Execute(t *testing.T) {
@@ -53,38 +53,38 @@ func Test_Execute(t *testing.T) {
 	name, _ := json.Marshal(param)
 
 	conn, err := sql.Open("mysql", string(name))
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	_, err = conn.Exec("CREATE DATABASE " + TestConnEnv.DbName)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	_, err = conn.Exec("USE " + TestConnEnv.DbName)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	_, err = conn.Exec(`CREATE TABLE test (
 		id INT PRIMARY KEY,
 		value VARCHAR(10)
 	)`)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	for i := 0; i < 10; i++ {
 		res, err := conn.Exec("INSERT INTO test VALUES(" + strconv.Itoa(i) + ",'" + strconv.Itoa(i) + "')")
-		unitest.NotError(t, err)
+		utest.IsNilNow(t, err)
 		num, _ := res.RowsAffected()
-		unitest.Pass(t, num == 1)
+		utest.Equal(t, num, 1)
 	}
 }
 
 func Test_Query(t *testing.T) {
 	conn, err := sql.Open("mysql", TestConnParam)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var res *sql.Rows
 
 	res, err = conn.Query("SELECT * FROM test ORDER BY id ASC")
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	defer res.Close()
 
 	i := 0
@@ -95,19 +95,19 @@ func Test_Query(t *testing.T) {
 		)
 
 		err := res.Scan(&id, &value)
-		unitest.NotError(t, err)
+		utest.IsNilNow(t, err)
 
-		unitest.Pass(t, id == int64(i))
-		unitest.Pass(t, value == strconv.Itoa(i))
+		utest.Equal(t, id, int64(i))
+		utest.Equal(t, value, strconv.Itoa(i))
 		i++
 	}
 
-	unitest.Pass(t, i == 10)
+	utest.Equal(t, i, 10)
 }
 
 func Test_Prepare(t *testing.T) {
 	conn, err := sql.Open("mysql", TestConnParam)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	var (
@@ -117,10 +117,10 @@ func Test_Prepare(t *testing.T) {
 	)
 
 	stmt, err = conn.Prepare("SELECT * FROM test ORDER BY id ASC")
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	reader, err = stmt.Query()
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	i := 0
 	for reader.Next() {
@@ -130,33 +130,33 @@ func Test_Prepare(t *testing.T) {
 		)
 
 		err := reader.Scan(&id, &value)
-		unitest.NotError(t, err)
+		utest.IsNilNow(t, err)
 
-		unitest.Pass(t, id == int64(i))
-		unitest.Pass(t, value == strconv.Itoa(i))
+		utest.Equal(t, id, int64(i))
+		utest.Equal(t, value, strconv.Itoa(i))
 		i++
 	}
 
-	unitest.Pass(t, i == 10)
+	utest.Equal(t, i, 10)
 
 	stmt.Close()
 
 	stmt, err = conn.Prepare("INSERT INTO test VALUES(?, ?)")
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 
 	res, err = stmt.Exec(10, "10")
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	num, _ := res.RowsAffected()
-	unitest.Pass(t, num == 1)
+	utest.Equal(t, num, 1)
 
 	stmt.Close()
 }
 
 func Test_Clean(t *testing.T) {
 	conn, err := sql.Open("mysql", TestConnParam)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 	defer conn.Close()
 
 	_, err = conn.Exec("DROP DATABASE " + TestConnEnv.DbName)
-	unitest.NotError(t, err)
+	utest.IsNilNow(t, err)
 }
